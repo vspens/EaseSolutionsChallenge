533348f80d1ae5f9332f4fa58af407aecbfdde46
ASI\vhistoria <vhistoria@csyoutsourcing.com>
2020-10-07 21:55:28 +0800

    Added partial logic Movements not final

--- src/impl/PathFinder.java
+++ src/impl/PathFinder.java
@@ -16,6 +16,9 @@ public class PathFinder {
         for(int row = 0; row < matrix.length; row++){
             for(int col = 0; col < matrix.length; col++){
                 pathInfo = new PathInfo();
+                Cell cell = new Cell(row, col);
+                pathInfo.getCellList().add(cell);
+                pathInfo.setCellList(pathInfo.getCellList());
                 pathInfo = this.getPath(matrix, row, col, pathInfo);
                 pathInfoList.add(pathInfo);
             }
@@ -24,46 +27,52 @@ public class PathFinder {
     }
 
     private PathInfo getPath(int[][] matrix, int row, int col, PathInfo pathInfo){
-        PathInfo pInfo = pathInfo;
+        PathInfo pInfoRight = pathInfo;
+        PathInfo pInfoLeft = pathInfo;
+        PathInfo pInfoDown = pathInfo;
+        PathInfo pInfoUp = pathInfo;
         int steep = 0;
         String direction = "right";
-
-        Cell cell = new Cell(row, col);
+        Cell cell;
 
         if(col + 1 < matrix.length && col + 1 < 1000 && matrix[row][col + 1] < matrix[row][col]){
             steep = matrix[row][col] - matrix[row][col+1];
+            cell = new Cell(row, col + 1);
             direction = "right";
-            pInfo.setSteep(steep);
-            pInfo.getCellList().add(cell);
-            pInfo.setCellList(pInfo.getCellList());
-            pInfo = moveToNextCell(direction, matrix, cell, pInfo);
+            pInfoRight.setSteep(steep);
+            pInfoRight.getCellList().add(cell);
+            pInfoRight.setCellList(pInfoRight.getCellList());
+            pInfoRight = moveToNextCell(direction, matrix, cell, pInfoRight);
         }
 
         if(col - 1 >= 0 && matrix[row][col - 1] < matrix[row][col]){
             steep = matrix[row][col] - matrix[row][col - 1];
+            cell = new Cell(row, col - 1);
             direction = "left";
-            pInfo.setSteep(steep);
-            pInfo.getCellList().add(cell);
-            pInfo.setCellList(pInfo.getCellList());
-            pInfo = moveToNextCell(direction, matrix, cell, pInfo);
+            pInfoLeft.setSteep(steep);
+            pInfoLeft.getCellList().add(cell);
+            pInfoLeft.setCellList(pInfoLeft.getCellList());
+            pInfoLeft = moveToNextCell(direction, matrix, cell, pInfoLeft);
         }
 
         if(row + 1 < matrix.length && row + 1 < 1000 && matrix[row + 1][col] < matrix[row][col]){
             steep = matrix[row][col] - matrix[row + 1][col];
+            cell = new Cell(row + 1, col);
             direction = "down";
-            pInfo.setSteep(steep);
-            pInfo.getCellList().add(cell);
-            pInfo.setCellList(pInfo.getCellList());
-            pInfo = moveToNextCell(direction, matrix, cell, pInfo);
+            pInfoDown.setSteep(steep);
+            pInfoDown.getCellList().add(cell);
+            pInfoDown.setCellList(pInfoDown.getCellList());
+            pInfoDown = moveToNextCell(direction, matrix, cell, pInfoDown);
         }
 
         if(row - 1 >= 0 && matrix[row - 1][col] < matrix[row][col]){
             steep = matrix[row][col] - matrix[row - 1][col];
+            cell = new Cell(row - 1, col);
             direction = "up";
-            pInfo.setSteep(steep);
-            pInfo.getCellList().add(cell);
-            pInfo.setCellList(pInfo.getCellList());
-            pInfo = moveToNextCell(direction, matrix, cell, pInfo);
+            pInfoUp.setSteep(steep);
+            pInfoUp.getCellList().add(cell);
+            pInfoUp.setCellList(pInfoUp.getCellList());
+            pInfoUp = moveToNextCell(direction, matrix, cell, pInfoUp);
         }
 
         return pathInfo;
