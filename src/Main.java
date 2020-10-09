3a2779cf696cec5d65489128fc10e02908e67886
ASI\vhistoria <vhistoria@csyoutsourcing.com>
2020-10-10 03:17:18 +0800

    done

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import object.PathInfo;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         try {
             int[][] matrix = Matrix.getGivenMatrix();
